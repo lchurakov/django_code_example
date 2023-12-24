e14550ed7d14fe2e65861e3df6a704a1bda4f883
Leonid Churakov <l.churakov@localhost.localdomain>
2023-12-24 17:09:51 +0400

    002: configured dotenv and added postgres constring

--- django_code_example/settings.py
+++ django_code_example/settings.py
@@ -11,22 +11,31 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+from os import environ as env
+
+load_dotenv()
+
+DJANGO_KEY = env.get("DJANGO_KEY")
+DB_URL = env.get("DB_URL")
+DB_USER = env.get("DB_USER")
+DB_NAME = env.get("DB_NAME")
+DB_PASSWORD = env.get("DB_PASSWORD")
+DEBUG_MODE = env.get("DEBUG_MODE")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8qgk$^#soe+bzw!#o8ys^*=zsfv+kcyxdfq(abe^+-ttvgfmdy'
+SECRET_KEY = DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG_MODE
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -54,8 +63,7 @@ ROOT_URLCONF = 'django_code_example.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,17 +78,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_code_example.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_URL,
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -111,7 +120,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
